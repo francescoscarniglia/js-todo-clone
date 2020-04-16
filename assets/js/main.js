d56aad51e0a1dd940d7f248e948977583b389583
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-04-16 15:44:00 +0200

    Html Css ok

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+console.log('ciao');
+
+// Esercizio di oggi: Todo list
+// Descrizione
+// Ricreare la todo list vista oggi durante la lezione
+// (focus dell’esercizio la logica)
+// Usiamo i template in html e il clone per separare
+// la parte logica (JS) dalla parte di markup (HTML)
+// recap funzionalità: mostrare, aggiungere e eliminare elementi
+// dalla lista
+// Nome della repo per consegnare l’esercizio: js-todo-clone
+// Link utili:
+// Doc metodo .on() https://api.jquery.com/on/#on-events-selector-data-handler
